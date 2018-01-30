0e27577574055c7e279376765c699ea2883027ce
Dani Cupples <danielle.volker@gmail.com>
2018-01-30 17:54:22 -0500

    create frame of application and initial component

---
+++ src/components/search_bar.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+//define search bar component
+class SearchBar extends Component {
+  render() {
+    return <input />;
+  }
+}
+
+//export code so that it's available to index.js
+export default SearchBar;
